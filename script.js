3771f0ca53d79f9d4d6a13be4b5a30105d7d22dd
SireneNouira <sirene.nouira16@gmail.com>
2024-11-26 12:27:39 +0100

    faire la promesse pour play

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-// AVEC LA SOURIS
-
 const audios = document.querySelectorAll("audio");
-// const key = document.querySelectorAll(".key");
-
-// key.forEach((key) => {
-//   key.addEventListener("mousedown", playSound);
-//   key.addEventListener("mouseleave", removeTransition);
-// });
-
-// function playSound(event) {
-//   const keyValue = this.dataset.key;
-//   const audio = [...audios].find((audio) => audio.dataset.key === keyValue);
-//   if (audio) {
-//     audio.currentTime = 0;
-//     audio.play();
-//     event.target.classList.add("playing");
-//   }
-// }
-// function removeTransition(event) {
-//   event.target.classList.remove("playing");
-// }
 
 const noteJouer = new Set();
 const noteEnregistrer = [];
 let recording = false;
 let playing = false;
-// AVEC LE KEYBOARD
+let timeCode = [];
+
 document.addEventListener("keydown", playSound);
 document.addEventListener("keyup", removeTransition);
 
 function playSound(event) {
+  
   // if(noteEnregistrer[0]) {
   //   const {keyCode, timeCode} = noteEnregistrer[0];
   //   console.log(timeCode);
-    
+
   // }
 
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
@@ -56,15 +37,18 @@ function playSound(event) {
   }
 
   if (recording === true) {
+    timeCode.push(Date.now());
+    let premiereTemps = timeCode[0];
+
+    timeCode.forEach((temps) => {
+      interval = temps - premiereTemps;
+    });
     noteEnregistrer.push({
       keyCode: event.keyCode,
-      timeCode: Date.now(),
+      interval,
     });
   }
   // console.log(noteEnregistrer);
-  
-  
-  // noteJouer.push(event);
 }
 function removeTransition(event) {
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
@@ -75,32 +59,48 @@ function removeTransition(event) {
   key.classList.toggle("playing");
 }
 
-//RECORD ET PLAY §§
+// PLAY §§
 
 let record = document.querySelector("#record");
 let play = document.querySelector("#play");
 record.addEventListener("click", handleRecord);
-play.addEventListener("click", handlePlay);
+play.addEventListener("click", handlePlayAsync);
 
 function handleRecord() {
   record.classList.toggle("playing");
   recording = !recording;
-  console.log("recording : " + recording);
+  // console.log("recording : " + recording);
 }
 
+function handlePlayAsync() {
+  noteEnregistrer.forEach((element) => {
+    setTimeout(() => {
+      
+      const keyboardEvent = new KeyboardEvent("keydown", {keyCode: element.keyCode});
+      const keyboardEvent2 = new KeyboardEvent("keyup", {keyCode: element.keyCode});
+      document.dispatchEvent(keyboardEvent);
+      setTimeout(() => {
+        document.dispatchEvent(keyboardEvent2);
+      }, 300)
+    }, element.interval);
+  });
+    play.classList.toggle("playing");
+    playing = !playing;
+  
 
-// function handlePlay(){
+  // const promesse = new Promise((resolve, reject) => {
+
+  // })
+}
+
+// function handlePlay() {
 //   play.classList.toggle("playing");
 //   playing = !playing;
-//   // for(let i =0; i<noteEnregistrer.length; i++){
-//   //   noteEnregistrer;
-//   // }
-//   if (noteEnregistrer>0) {
-//     // audio.currentTime = 0;
+
+//   if (noteEnregistrer > 0) {
+//     noteEnregistrer.setTimeout(timeCode);
+//     noteEnregistrer.currentTime = 0;
 //     noteEnregistrer.play();
-//     key.classList.toggle("playing");
+//     // console.log(noteEnregistrer);
 //   }
 // }
-
-
-
